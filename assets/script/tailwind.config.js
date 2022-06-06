cd5377e2e20973755ec4a96102f5b3c7b040d02a
Sandesh Jain <sandeshjain2507@gmail.com>
2022-06-06 16:59:35 +0530

    Initial Commit

---
+++ assets/script/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            headingColor : '#FDFDFD',
+            textColor : "#F1F1F1",
+            primaryDark : "#0F0F0F",
+            gray1: "#4D4D4D",
+            gray2 : "#313131",
+            darkGradient1 : "#161616",
+            darkGradient2 : "#0F0F0F",
+            orangeGradient1: "#FF834F",
+            orangeGradient2: "#FF5E11",
+        },
+      }
+    }
+  }
